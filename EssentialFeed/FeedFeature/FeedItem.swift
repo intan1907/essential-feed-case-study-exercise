acdff215cfbd9815b91287478c8760f732ac1b27
Intan Nurjanah <intan3951@gmail.com>
2022-10-09 05:46:50 +0700

    Initial commit with feature requirements

---
+++ EssentialFeed/FeedFeature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Intan Nurjanah on 07/10/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
