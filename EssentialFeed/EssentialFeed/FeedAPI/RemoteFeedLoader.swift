22e4da47535fbdcac26fcd9209daa2083fd7ffe5
Intan Nurjanah <intan3951@gmail.com>
2022-10-10 14:55:40 +0700

    Move Decodable logic to a new private item struct to decouple the Feed Feature module from API implementation details

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -40,7 +40,7 @@ public final class RemoteFeedLoader { // prevent subclasses
             switch result {
             case .success(let data, let response):
                 if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items))
+                    completion(.success(root.items.map { $0.item }))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -52,5 +52,23 @@ public final class RemoteFeedLoader { // prevent subclasses
 }
 
 private struct Root: Decodable {
-    let items: [FeedItem]
+    let items: [Item]
+}
+
+// buat FeedItem model terpisah khusus untuk DAO
+private struct Item: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL // namanya sama dengan yang ada di response
+    
+    // mapper ke domain model
+    var item: FeedItem {
+        return FeedItem(
+            id: id,
+            description: description,
+            location: location,
+            imageURL: image
+        )
+    }
 }
