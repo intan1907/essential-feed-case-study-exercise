af05da4b5ea5172b266cba1ee16ea59fdff4f299
Intan Nurjanah <intan3951@gmail.com>
2022-10-10 17:17:59 +0700

    Move all mapping logic to to the FeedItemsMapper

--- EssentialFeed/EssentialFeed/FeedAPI/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/FeedItemsMapper.swift
@@ -12,6 +12,10 @@ internal final class FeedItemsMapper {
     // move the Root and Item structs here so no one as access to it
     private struct Root: Decodable {
         let items: [Item]
+        
+        var feed: [FeedItem] {
+            return items.map { $0.item }
+        }
     }
 
     // buat FeedItem model terpisah khusus untuk DAO
@@ -33,13 +37,15 @@ internal final class FeedItemsMapper {
     }
     
     private static var OK_200: Int { return 200 }
-
-    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
+    
+    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard
+            response.statusCode == OK_200,
+            let root = try? JSONDecoder().decode(Root.self, from: data)
+        else {
+            return .failure(.invalidData)
         }
         
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.items.map { $0.item }
+        return .success(root.feed)
     }
 }
