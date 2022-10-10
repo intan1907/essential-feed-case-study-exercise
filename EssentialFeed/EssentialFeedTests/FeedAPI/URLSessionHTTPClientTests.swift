6a0582175779da1f14d13ec97f4555c1df91094c
Intan Nurjanah <intan3951@gmail.com>
2022-10-11 05:34:07 +0700

    Creates URLSession data task with URL on `URLSessionHTTPClient.get(from: URL)`

---
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -0,0 +1,51 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeedTests
+//
+//  Created by Intan Nurjanah on 11/10/22.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in }
+    }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+    
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    
+    // subclass-based mocking
+    // - it can be dangerous if we don't own those classes and we can't have access to the implementation
+    // - if we mocking class we don't own, we can start creating assumptions in our mocked behavior that could be wrong
+    // - plus this class have a bunch of methods that we aren't overriding here and we never know how these methods might be interoperating between them
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
+    
+}
