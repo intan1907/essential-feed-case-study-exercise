cecae1e0cdc950cf45a0d508f447bc996921db95
Intan Nurjanah <intan3951@gmail.com>
2022-10-19 05:25:47 +0700

    Move `FeedCachePolicy` to its own file

--- EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -7,32 +7,6 @@
 
 import Foundation
 
-// Rules and policies (e.g., validation logic) are better suited in a Domain Model that is application-agnostic (so it can be [re]used across applications).
-private final class FeedCachePolicy {
-    // The `currentDate` function is impure because everytime you invoke this function it may return a different value. It's non-deterministic.
-    // It's easier to reason about core logic that is deterministic.
-    private static let calendar = Calendar(identifier: .gregorian)
-    
-    // The initializers is private because `FeedCachePolicy` is a rule (value object: a model without identity), not a model with indentity. It just holds values, not a state.
-    // So you can never have an instance of this.
-    private init() { }
-    // - entity -> model with identity -> have a state -> can be instantiate
-    // - value object -> model without identity -> have no state -> can never be instantiated -> preferred to be static
-    
-    private static var maxCacheAgeInDays: Int {
-        return 7
-    }
-    
-    // So we remove the `currentDate` function and deal only with values
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        // given date + max days
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
-            return false
-        }
-        return date < maxCacheAge
-    }
-}
-
 // The `LocalFeedLoader` should encapsulate application-specific logic only, and communicate with Models to perform business logic.
 public final class LocalFeedLoader {
     private let store: FeedStore
