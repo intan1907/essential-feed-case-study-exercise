c944bd5baeaf67c28fe1aba5912498e22f4091b0
Intan Nurjanah <intan3951@gmail.com>
2022-10-24 06:07:49 +0700

    Replace occurrences of `Error?` for representing operation success/failure with `Result<Void, Error>`

--- EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -12,10 +12,10 @@ public typealias CachedFeed = (feed: [LocalFeedImage], timestamp: Date)
 // To decouple the application from framework details, we don't let frameworks dictate the Use Case interface (e.g., adding Codable requirements or CoreData managed contexts parameters).
 // We do so by test-driving the interfaces the Use Case needs for its collaborators, rather than defining the interface upfront to facilitate a specific framework implementation.
 public protocol FeedStore {
-    typealias DeletionResult = Error?
+    typealias DeletionResult = Result<Void, Error>
     typealias DeletionCompletion = (DeletionResult) -> Void
     
-    typealias InsertionResult = Error?
+    typealias InsertionResult = Result<Void, Error>
     typealias InsertionCompletion = (InsertionResult) -> Void
     
     typealias RetrievalResult = Result<CachedFeed?, Error>
