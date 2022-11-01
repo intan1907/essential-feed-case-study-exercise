ea5b8d60f89dfd5532fe38adf8e4f6cacb36455c
Intan Nurjanah <intan3951@gmail.com>
2022-11-01 13:50:39 +0700

    Extract duplicate logic into a reusable `UIControl` extension

--- EssentialFeed/EssentialFeediOSTests/Helpers/UIRefreshControl+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIRefreshControl+TestHelpers.swift
@@ -8,12 +8,7 @@
 import UIKit
 
 extension UIRefreshControl {
-    // iterate through all the targets and actions for the `.valueChanged` event in the refresh control and perform the action
     func simulatePullToRefresh() {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
+        simulate(event: .valueChanged)
     }
 }
