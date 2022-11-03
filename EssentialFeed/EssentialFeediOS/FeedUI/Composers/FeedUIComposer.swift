24a1e3d14395e5899e3da3b5eda13cd17be1e3ce
Intan Nurjanah <intan3951@gmail.com>
2022-11-04 05:43:43 +0700

    Move `WeakRefVirtualProxy` to separate file

--- EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -42,27 +42,6 @@ extension FeedViewController {
     }
 }
 
-// membuat suatu object `T` menjadi weak reference
-private final class WeakRefVirtualProxy<T: AnyObject> {
-    private weak var object: T?
-    
-    init(_ object: T) {
-        self.object = object
-    }
-}
-
-extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
-    func display(_ viewModel: FeedLoadingViewModel) {
-        object?.display(viewModel)
-    }
-}
-
-extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
-    func display(_ viewModel: FeedImageViewModel<UIImage>) {
-        object?.display(viewModel)
-    }
-}
-
 // this is an Adapter pattern and it's a common pattern you will encounter on a composer
 // when composing type, the Adapter pattern helps you connect unmatching APIs
 // [FeedImage] -> Adapt -> [FeedImageCellController]
---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/WeakRefVirtualProxy.swift
@@ -0,0 +1,29 @@
+//
+//  WeakRefVirtualProxy.swift
+//  EssentialFeediOS
+//
+//  Created by Intan Nurjanah on 04/11/22.
+//
+
+import UIKit
+
+// membuat suatu object `T` menjadi weak reference
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ viewModel: FeedImageViewModel<UIImage>) {
+        object?.display(viewModel)
+    }
+}
