808c6ca80e888e47aa75d6f427722060e2ae516b
Intan Nurjanah <intan3951@gmail.com>
2022-11-03 19:16:12 +0700

    Create test helper to find missing localized strings

--- EssentialFeed/EssentialFeediOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
@@ -17,16 +17,7 @@ class FeedViewControllerTests: XCTestCase {
         
         sut.loadViewIfNeeded()
         
-        let bundle = Bundle(for: FeedViewController.self)
-        let localizedKey = "FEED_VIEW_TITLE"
-        // `localizedString` parameters:
-        // - key: the key
-        // - value: default value if the desired value not found. If `nil`, it will give the `key` as the default value
-        // - table: `.strings` file name that will be the place to search on. If `nil`, it will search on all table
-        let localizedTitle = bundle.localizedString(forKey: localizedKey, value: nil, table: "Feed")
-        
-        XCTAssertNotEqual(localizedKey, localizedTitle, "Missing localized string for key: \(localizedKey)")
-        XCTAssertEqual(sut.title, localizedTitle)
+        XCTAssertEqual(sut.title, localized("FEED_VIEW_TITLE"))
     }
     
     func test_loadFeedActions_requestFeedFromLoader() {
---
+++ EssentialFeed/EssentialFeediOSTests/FeedUI/Controllers/Helpers/FeedViewControllerTests+Localization.swift
@@ -0,0 +1,25 @@
+//
+//  FeedViewControllerTests+Localization.swift
+//  EssentialFeediOSTests
+//
+//  Created by Intan Nurjanah on 03/11/22.
+//
+
+import XCTest
+import EssentialFeediOS
+
+extension FeedViewControllerTests {
+    func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: FeedViewController.self)
+        // `localizedString` parameters:
+        // - key: the key
+        // - value: default value if the desired value not found. If `nil`, it will give the `key` as the default value
+        // - table: `.strings` file name that will be the place to search on. If `nil`, it will search on all table
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+}
