5cf7caaf8da2a4d77cfd6b3c6ec98fc49ea71ec8
Intan Nurjanah <intan3951@gmail.com>
2022-11-05 16:00:16 +0700

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Intan Nurjanah on 05/11/22.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
