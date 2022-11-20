01f7ea6ceb506331b5924f30787093cde5a9b451
Intan Nurjanah <intan3951@gmail.com>
2022-11-20 17:18:23 +0700

    `FeedImageDataLoaderCacheDecorator.loadImageData` caches loaded data on loader success

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -8,22 +8,35 @@
 import XCTest
 import EssentialFeed
 
+protocol FeedImageDataCache {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func insert(_ data: Data, for url: URL, completion: @escaping (Result) -> Void)
+}
+
 class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
     private let decoratee: FeedImageDataLoader
+    private let cache: FeedImageDataCache
     
-    init(decoratee: FeedImageDataLoader) {
+    init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
         self.decoratee = decoratee
+        self.cache = cache
     }
     
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        return decoratee.loadImageData(from: url, completion: completion)
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            if let data = try? result.get() {
+                self?.cache.insert(data, for: url) { _ in }
+            }
+            completion(result)
+        }
     }
 }
 
 class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
     
     func test_loadImageData_deliversDataOnLoaderSuccess() {
-        let (sut, loader) = makeSUT()
+        let (sut, loader, _) = makeSUT()
         let data = anyData()
         
         expect(sut, toCompleteWith: .success(data), when: {
@@ -32,21 +45,36 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
     }
     
     func test_loadImageData_deliversErrorOnLoaderFailure() {
-        let (sut, loader) = makeSUT()
+        let (sut, loader, _) = makeSUT()
         
         expect(sut, toCompleteWith: .failure(anyNSError()), when: {
             loader.complete(with: anyNSError())
         })
     }
     
+    func test_loadImageData_cachesLoadedDataOnLoaderSuccess() {
+        let (sut, loader, cache) = makeSUT()
+        let url = anyURL()
+        let data = anyData()
+        
+        let exp = expectation(description: "Waiting for load completion")
+        _ = sut.loadImageData(from: url) { _ in exp.fulfill() }
+        
+        loader.complete(with: data)
+        
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(cache.messages, [.insert(data, url)], "Expected to cache loaded data on success")
+    }
+    
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImageDataLoader, loader: LoaderSpy) {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImageDataLoader, loader: LoaderSpy, cache: CacheSpy) {
+        let cache = CacheSpy()
         let loader = LoaderSpy()
-        let sut = FeedImageDataLoaderCacheDecorator(decoratee: loader)
+        let sut = FeedImageDataLoaderCacheDecorator(decoratee: loader, cache: cache)
         trackForMemoryLeaks(loader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
-        return (sut, loader)
+        return (sut, loader, cache)
     }
     
     private func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
@@ -90,4 +118,17 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
             messages[index].completion(.success(imageData))
         }
     }
+    
+    private class CacheSpy: FeedImageDataCache {
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            case insert(Data, URL)
+        }
+        
+        func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataCache.Result) -> Void) {
+            messages.append(.insert(data, url))
+        }
+    }
+    
 }
