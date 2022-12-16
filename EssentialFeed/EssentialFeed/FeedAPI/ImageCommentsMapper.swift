fcd36564aeeb388d4f72926567f3a6643b28be57
Intan Nurjanah <intan3951@gmail.com>
2022-12-16 12:50:00 +0700

    Duplicate `RemoteFeedLoader` as `RemoteImageCommentsLoader`

---
+++ EssentialFeed/EssentialFeed/FeedAPI/ImageCommentsMapper.swift
@@ -0,0 +1,22 @@
+//
+//  ImageCommentsMapper.swift
+//  EssentialFeed
+//
+//  Created by Intan Nurjanah on 16/12/22.
+//
+
+import Foundation
+
+final class ImageCommentsMapper {
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+        
+        return root.items
+    }
+}
