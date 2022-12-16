6e03bf562db24ee86f439ccd3f8971207a308b82
Intan Nurjanah <intan3951@gmail.com>
2022-12-16 18:09:03 +0700

    Replace `RemoteFeedLoader` with generic `RemoteLoader` to remove duplication

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -7,46 +7,10 @@
 
 import Foundation
 
-public final class RemoteFeedLoader: FeedLoader { // prevent subclasses
-    private let url: URL
-    private let client: HTTPClient
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public typealias Result = FeedLoader.Result
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { [weak self] result in
-            // bisa saja ada case di mana `client` hidup lebih lama dari `RemoteFeedLoader`
-            // tetapi client menyimpan instance dari `load(completion:)` dan meng-invoke-nya
-            // sedangkan kita tidak menginginkan behavior tsb
-            // sehingga kita meng-capture `self` dengan `weak self`
-            // lalu set guard untuk melanjutkan hanya jika `self` masih hidup
-            guard let self = self else { return }
-            
-            switch result {
-            case let .success((data, response)):
-                completion(self.map(data, from: response))
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
-    }
-    
-    private func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let items = try FeedItemsMapper.map(data, from: response)
-            return .success(items)
-        } catch {
-            return .failure(error)
-        }
+public typealias RemoteFeedLoader = RemoteLoader<[FeedImage]>
+
+extension RemoteFeedLoader {
+    public convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: FeedItemsMapper.map)
     }
 }
