b375270b16455b5605755dc06917dfe0056a53bc
Intan Nurjanah <intan3951@gmail.com>
2022-12-31 12:53:45 +0700

    Make `CoreData` `FeedStore` implementation sync

--- EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -32,11 +32,6 @@ public final class CoreDataFeedStore {
         }
     }
     
-    func performAsync(_ action: @escaping (NSManagedObjectContext) -> Void) {
-        let context = context
-        context.perform { action(context) }
-    }
-    
     func performSync<R>(_ action: (NSManagedObjectContext) -> Result<R, Error>) throws -> R {
         let context = context
         var result: Result<R, Error>!
